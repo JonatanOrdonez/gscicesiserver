6cbd7de919d50fe903e505fc052b413c594e6f79
JonatanOrdonez <jonatan-ordonez@hotmail.com>
2018-06-02 10:10:52 -0500

    asdfasd

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,24 +15,20 @@ class UsersTableSeeder extends Seeder
         $nombres = [
             0 => "salasclient",
             1 => "angularclient",
-            2 => "administrador"
         ];
         $emails = [
             0 => "salasclient@correo.icesi.edu.co",
             1 => "angularclient@correo.icesi.edu.co",
-            2 => "administrador@correo.icesi.edu.co"
         ];
         $api_tokens = [
             0 => 'tnrApcPohoar9padK6JTEY3Z8mm7OkzkyLYUXgzjnJbqOg3DT1tOIuhQKBfM',
             1 => '$2y$10$X0yixpWjRsiSkQyK6SoCN.dvmYVBWK9b9uTSOmpXws8ITpYr9y.yO',
-            2 => 'E3i3UkYkCo6G0GT7Cf7Dlnj4ql8RPSVEPJgZTnUm11CJ4L5dERCb3qLHKkzL'
         ];
         $passwords = [
             0 => 'salasclient',
             1 => 'angularclient',
-            2 => 'administrador'
         ];
-        $cantidad = 3;
+        $cantidad = 2;
         for ($i = 0; $i < $cantidad; $i++) {
             DB::table('users')->insert([
                 'name' => $nombres[$i],
